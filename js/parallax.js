c8f65c3b6b04009539b5818a6cdb71f827d4fa35
Alexandru Balan <32680943+thebalanitos@users.noreply.github.com>
2017-11-14 00:27:45 +0200

    Parallax 1

--- js/parallax.js
+++ js/parallax.js
@@ -5,7 +5,9 @@ function parallax(){
 	
 	var wScroll = $(window).scrollTop();
 
-	if($(window).width() <= 480)
+	if($(window).width() <= 375)
+		$('.parallax').css('background-position','center '+(wScroll)*0.126+'px');
+	if($(window).width() > 375 && $(window).width() <=480 )
 		$('.parallax').css('background-position','center '+(wScroll)*0.148+'px');
 	if($(window).width() > 768)
 		$('.parallax').css('background-position','center '+(wScroll)*0.43+'px');
